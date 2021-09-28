c0fb7595c65485c3f7739cd868e00b848ce7113b
Hendi Irwanto <hi.hendiirwanto@gmail.com>
2021-09-28 07:16:55 +0700

    add function for search friends

--- lib/app/modules/search/controllers/search_controller.dart
+++ lib/app/modules/search/controllers/search_controller.dart
@@ -1,8 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
   late TextEditingController searchController;
+
+  //membuat variable tipenya list/array untuk nantinya menapung hasil search
+  var queryAwal = [].obs;
+  var tempSearch = [].obs;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  //fungsi untuk mencari teman
+  void searchFriends(String data) async {
+    //mengecek apakah ada inputan di textfield
+    if (data.length == 0) {
+      queryAwal.value = [];
+      tempSearch.value = [];
+    } else {
+      //ini untuk membuat huruf pertama nama itu menjadi kapital
+      var capitalFirstLetter =
+          data.substring(0, 1).toUpperCase() + data.substring(1);
+
+      //mengecek apakah data query kosong dan inputan textfield hanya mengetik 1 huruf
+      if (queryAwal.length == 0 && data.length == 1) {
+        CollectionReference clients = await firestore.collection("clients");
+
+        //ini untuk mengecek apakah keyName sama dengan inputan pertama di textfield
+        final keyNameResult = await clients
+            .where("keyName", isEqualTo: data.substring(0, 1).toUpperCase())
+            .get();
+        //jika sama dan tidak kosong maka di looping untuk mendapatkan data query dan di simpan di queryAwal
+        if (keyNameResult.docs.length > 0) {
+          for (var i = 0; i < keyNameResult.docs.length; i++) {
+            queryAwal.add(keyNameResult.docs[i].data() as Map<String, dynamic>);
+          }
+        }
+      }
+      //mengecek apakah queryAwal tidak kosong
+      if (queryAwal.length != 0) {
+        //ini fungsi untuk looping array/list di data queryAwal
+        queryAwal.forEach((element) {
+          //ini fungsi untuk mengecek apakah data di list name queryAwal itu sama dengan inputan di textField
+          if (element["name"].startsWith(capitalFirstLetter)) {
+            //jika sama masukkan ke list tempSearch
+            tempSearch.add(element);
+          }
+        });
+      }
+    }
+    queryAwal.refresh();
+    tempSearch.refresh();
+  }
 
   @override
   void onInit() {
--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -7,33 +7,6 @@ import 'package:lottie/lottie.dart';
 import '../controllers/search_controller.dart';
 
 class SearchView extends GetView<SearchController> {
-  final List<Widget> myFriends = List.generate(
-    20,
-    (index) => ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset(
-          "assets/logo/noimage.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        "Orang ke ${index + 1}",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-      ),
-      subtitle: Text(
-        "orang${index + 1}@gmail.com",
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-      ),
-      trailing: GestureDetector(
-        onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-        child: Chip(
-          label: Text("Message"),
-        ),
-      ),
-    ),
-  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +26,7 @@ class SearchView extends GetView<SearchController> {
               child: TextField(
                 controller: controller.searchController,
                 cursorColor: Colors.pink[400],
+                onChanged: (value) => controller.searchFriends(value),
                 decoration: InputDecoration(
                   fillColor: Colors.white,
                   filled: true,
@@ -88,23 +62,44 @@ class SearchView extends GetView<SearchController> {
         ),
         preferredSize: Size.fromHeight(140),
       ),
-      body: myFriends == 0
-          ? Center(
-              child: Container(
-                width: Get.width * 0.7,
-                height: Get.width * 0.7,
-                child: Lottie.asset(
-                  "assets/lottie/empty.json",
+      body: Obx(
+        () => controller.tempSearch.length == 0
+            ? Center(
+                child: Container(
+                  width: Get.width * 0.7,
+                  height: Get.width * 0.7,
+                  child: Lottie.asset("assets/lottie/empty.json"),
+                ),
+              )
+            : ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: controller.tempSearch.length,
+                itemBuilder: (BuildContext context, int index) => ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.black26,
+                    child: Image.asset(
+                      "assets/logo/noimage.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  title: Text(
+                    "${controller.tempSearch[index]["name"]}",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  subtitle: Text(
+                    "${controller.tempSearch[index]["email"]}",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                  trailing: GestureDetector(
+                    onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                    child: Chip(
+                      label: Text("Message"),
+                    ),
+                  ),
                 ),
               ),
-            )
-          : ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myFriends.length,
-              itemBuilder: (BuildContext context, int index) {
-                return myFriends[index];
-              },
-            ),
+      ),
     );
   }
 }
